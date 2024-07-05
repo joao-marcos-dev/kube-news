7c78c6c2f84884e9902b8e0e32243b68e7860a49
joao.marcos <marcosrodrigues200@msn.com>
2024-07-05 11:01:06 -0300

    Estudando docker e docker compose

--- src/models/post.js
+++ src/models/post.js
@@ -1,49 +1,54 @@
-const sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 
-const DB_DATABASE = process.env.DB_DATABASE || "kubedevnews";
-const DB_USERNAME = process.env.DB_USERNAME || "kubedevnews";
-const DB_PASSWORD = process.env.DB_PASSWORD || "Pg#123";
+const DB_DATABASE = process.env.DB_DATABASE || "kubenews";
+const DB_USERNAME = process.env.DB_USERNAME || "postgres";
+const DB_PASSWORD = process.env.DB_PASSWORD || "Pg123";
 const DB_HOST = process.env.DB_HOST || "localhost";
+const DB_PORT = process.env.DB_PORT || 5435;
 
-const seque = new sequelize.Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
-    host: DB_HOST,
-    dialect: 'postgres'
-  });
+const sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
+  dialect: 'postgres',
+  host: DB_HOST,
+  port: DB_PORT
+});
 
-class Post extends sequelize.Model {
-  
+class Post extends Sequelize.Model {
+
   save() {
-    
-    console.log('Entrou')
-    super.save();
+    console.log('Entrou');
+    return super.save();
   }
 }
 
 Post.init({
   title: {
-    type: sequelize.DataTypes.STRING,
-    require: true
+    type: Sequelize.DataTypes.STRING,
+    allowNull: false, // Use allowNull instead of require for boolean values
   },
   summary: {
-    type: sequelize.DataTypes.STRING,
-    require: true
+    type: Sequelize.DataTypes.STRING,
+    allowNull: false,
   },
   publishDate: {
-    type: sequelize.DataTypes.DATEONLY,
-    require: true
+    type: Sequelize.DataTypes.DATEONLY,
+    allowNull: false,
   },
   content: {
-    type: sequelize.DataTypes.STRING(2000),
-    require: true
+    type: Sequelize.DataTypes.STRING(2000),
+    allowNull: false,
   },
 }, {
-  sequelize: seque, // We need to pass the connection instance
+  sequelize, // We need to pass the connection instance
   modelName: 'Post' // We need to choose the model name
-})
+});
 
-exports.initDatabase = () => {
-    seque.sync({ alter: true })
-}
+exports.initDatabase = async () => {
+  try {
+    await sequelize.sync({ alter: true });
+    console.log('Database synchronized successfully');
+  } catch (error) {
+    console.error('Error synchronizing database:', error);
+  }
+};
 
 exports.Post = Post;
-
